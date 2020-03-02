12e917c39aaa33d32b49ec656a661841651c1e9c
Donny L <donny.landis@gmail.com>
2020-03-02 00:24:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,6 +34,6 @@ def retrieve_last_element_from_array(array)
   array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-  array[index_number] = element
+def update_element_from_index(x, y, z)
+  x[y] = z
 end
